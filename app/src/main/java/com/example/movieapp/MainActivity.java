fe068d605695c5eefd64b2050a2904529fb9b5b4
AksharaaRaghu <akshararaghu2004@gmail.com>
2024-12-17 12:41:29 +0530

    dashboard in progress

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 getName=et1.getText().toString();
                 getPass=et2.getText().toString();
+                if(getName.equals("theatre")&&getPass.equals("1234")){
+                    Intent ob=new Intent(getApplication(),DashboardActivity.class);
+                            startActivity(ob);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"invalid credentials",Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
